796c8e9da3a4d135ff8275b639e9066c0e24be71
Yahya Zakrya khan <yahyazakryakhan@gmail.com>
2023-11-02 02:52:04 +0300

    ....

---
+++ Recap_Demo_1/Recap_Demo_1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.add();
+            customerManager.update();
+            
+            ProductManager productManager = new ProductManager();
+            productManager.add();
+            productManager.update();   
+            
+            Customer customer = new Customer();
+            customer.City = "Karabük";
+            customer.Id = 1;
+            customer.FirstName = "Yahya";
+            customer.LastName = "Zakrya Khan";
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2, City = "Karabük", FirstName = "Yahya", LastName = "Zakrya Khan"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+
+        }
+    }
+    class CustomerManager
+    {
+        public void add()
+        {
+            Console.WriteLine("Customer added");
+        }
+        public void update()
+        {
+            Console.WriteLine("Customer updated");
+        }
+    }
+    class ProductManager
+    {
+        public void add()
+        {
+            Console.WriteLine("Product added");
+        }
+        public void update()
+        {
+            Console.WriteLine("Pruduct updated");
+        }
+    }
+
+    class Customer
+    {
+        //Property
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+       /* private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return "Mr." + _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }*/
+        public string LastName { get; set; }
+         public string City { get; set; }
+    }      
+}
