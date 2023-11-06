203edf77462ecd2ab0c242af97498084bc8e7ea9
Yahya Zakrya khan <yahyazakryakhan@gmail.com>
2023-11-06 16:50:57 +0300

    ......

---
+++ Birthdates/Birthdates/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthdates
+{
+    class Users
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Users> user = new List<Users>()
+            {
+                new Users{FirstName = "Yahya", LastName = "Zakrya Khan", BirthDate = new DateTime(2000, 04, 14)},
+                new Users{FirstName = "Bilal", LastName = "Çavuş", BirthDate = new DateTime(2000, 11, 30)},
+                new Users{FirstName = "Yusuf", LastName = "Bakkal", BirthDate = new DateTime(2001, 09, 28)},
+                new Users{FirstName = "Kadir", LastName = "Albayrak", BirthDate = new DateTime(2001, 09, 25)},
+                new Users{FirstName = "Ismail Enes", LastName = "Yardibi", BirthDate = new DateTime(2001, 07, 31)},
+                new Users{FirstName = "Bora Sezer", LastName = "Çetin", BirthDate = new DateTime(2001, 07, 09)},
+                new Users{FirstName = "Ayşe", LastName = "Ahmetoğlları", BirthDate = new DateTime(2003, 04, 28)},
+                new Users{FirstName = "Rabia", LastName = "Ahmetoğulları", BirthDate = new DateTime(2001, 10, 27)},
+                new Users{FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1998, 04, 04)},
+                new Users{FirstName = "Dwayne", LastName = "Johnson", BirthDate = new DateTime(1980, 04, 14)}
+
+            };
+
+            DateTime present = DateTime.Now;
+
+            foreach(var u in user)
+            {
+                int year = present.Year - u.BirthDate.Year;
+                if (present < u.BirthDate.AddYears(year)) year--;
+                Console.WriteLine(u.FirstName + " " + u.LastName + " - Years old : " + year );
+            }
+
+            user.Sort((x, y) => x.BirthDate.CompareTo(y.BirthDate));
+
+            Console.Write("\n\n\n");
+
+            Console.WriteLine("Sorted List :");
+
+            foreach(var u in user)
+            {
+                Console.WriteLine(u.FirstName + " " + u.LastName + " " + u.BirthDate.ToString("yyyy-MM-dd"));
+            }
+        }
+    }
+}
---
+++ Collections/Koleksiyonlar/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList();
+            List();
+
+
+           
+            Console.ReadLine();
+        }
+        private static void ArrayList()
+        { 
+            var cities = new ArrayList();
+            cities.Add("Ankara");
+            cities.Add("Adana");
+
+            foreach (var c in cities)
+            {
+                Console.WriteLine(c);
+            }
+            cities.Add("Istanbul");
+            Console.WriteLine(cities[2]);
+           
+
+        }
+        private static void List()
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("Istnabul");
+
+            //Console.WriteLine(cities.Contains("Ankara")); TRUE, FALSE
+
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer{Id = 1, Firstname = "Yahya", Lastname = "Zakrya Khan"}
+
+            };
+            
+            var customer = new Customer
+            {
+                Id = 3,
+                Firstname = "Salih",
+                Lastname = "Emini"
+            };
+            customers.Add(customer);
+            customers.AddRange(new Customer[2]
+            {
+                new Customer { Id = 4, Firstname = "Ali", Lastname = "sksksk"},
+                new Customer { Id = 5, Firstname = "Ali", Lastname = "sksksk"}
+                
+
+            });
+            var Index =  customers.IndexOf(customer);
+            Console.WriteLine("Index : {0} ",Index);
+           // customers.Clear();
+            
+            //customers.Add(new Customer { Id = 1, Firstname = "Yahya", Lastname = "Zakrya Khan" });
+            foreach(var c in customers)
+            {
+                Console.WriteLine(c.Firstname);
+            }
+            var count = customers.Count();
+            Console.WriteLine("Count : {0}", count);
+
+        }
+    }
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
