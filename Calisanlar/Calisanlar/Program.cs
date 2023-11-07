113336d3e7337582373e081cc3783dcf23fa06da
Yahya Zakrya khan <yahyazakryakhan@gmail.com>
2023-11-07 20:03:48 +0300

    ..............

---
+++ Calisanlar/Calisanlar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calisanlar
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> AdveSoyad = new List<string>();
+            List<double> Maas = new List<double>();
+
+            for(int i = 0; i < 10;i++) {
+                Console.WriteLine($"Lütfen {i+1}. kişinin adını ve soyadını girin:");
+                string adSoyad = Console.ReadLine();
+                AdveSoyad.Add(adSoyad);
+
+                Console.WriteLine($"Lütfen {i+1}. kişinin maaşını girin :");
+                double maas = Convert.ToDouble(Console.ReadLine());
+                Maas.Add(maas);
+            }
+            HesaplaVeSirala(AdveSoyad, Maas);
+
+            Console.WriteLine("Maaşlar ve isimler sıralandı");
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine($"{AdveSoyad[i]}, {Maas[i]}");
+            }
+        }
+        
+        static void HesaplaVeSirala(List<string>AdveSoyad, List<double> Maas)
+        {
+            for(int i = 0; i < 10; i++) {
+                double yeniMaas = Maas[i] * 95; //%5 kesinti yapılır
+                Maas[i] = yeniMaas;
+            }
+
+            //Maaşları ve isimleri sırala
+            for (int i = 0; i < 9; i++)
+            {
+                for(int j = i + 1; j < 10; j++)
+                {
+                    if (Maas[i] < Maas[j]) {
+                        double temp = Maas[i];
+                        Maas[i] = Maas[j];
+                        Maas[j] = temp;
+
+                        string tempAd = AdveSoyad[i];
+                        AdveSoyad[i] = AdveSoyad[j];
+                        AdveSoyad[j] = tempAd;
+                    }
+                }
+            }
+        }
+
+    }
+   
+    }
+
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Program
+    {
+        enum Calisanlar : byte
+        {
+            idareci, //Çalışan tipi 0
+            ogretmen, //Çalışan tipi 1
+            memur, //Çalışan tipi 2
+            hizmetli //Çalışan tipi 3
+        }
+        static void Main(string[] args)
+        {
+            int CalisanTipi;
+            Console.WriteLine("çalışan tipini giriniz :");
+            CalisanTipi = Convert.ToInt32(Console.ReadLine());
+
+            switch(CalisanTipi)
+            {
+                case 0:
+                    Console.WriteLine("Çalışan tipi : " + Calisanlar.idareci + " Maaş : 100TL");
+                    break;
+                case 1:
+                    Console.WriteLine("Çalışan tipi : " + Calisanlar.ogretmen + " Maaş : 200TL");
+                    break;
+                case 2:
+                    Console.WriteLine("Çalışan tipi : " + Calisanlar.memur + " Maaş : 300Tl");
+                    break;
+                case 3:
+                    Console.WriteLine("Çalışan tipi : " + Calisanlar.hizmetli + " Maaş : 400TL");
+                    break;
+            }
+        }
+    }
+}
