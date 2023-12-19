f35de154610de28e2d2aefb38b5c89727f2ff6bf
Yahya Zakrya khan <yahyazakryakhan@gmail.com>
2023-12-19 20:02:36 +0300

    SQLite

---
+++ Veritabani/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textSurname = new System.Windows.Forms.TextBox();
+            this.textMail = new System.Windows.Forms.TextBox();
+            this.comboLevel = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.level = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(50, 59);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(50, 88);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(50, 117);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(50, 146);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(375, 66);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(44, 16);
+            this.Label1.TabIndex = 4;
+            this.Label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(375, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Level";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(456, 63);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(149, 22);
+            this.textName.TabIndex = 8;
+            // 
+            // textSurname
+            // 
+            this.textSurname.Location = new System.Drawing.Point(456, 92);
+            this.textSurname.Name = "textSurname";
+            this.textSurname.Size = new System.Drawing.Size(149, 22);
+            this.textSurname.TabIndex = 9;
+            // 
+            // textMail
+            // 
+            this.textMail.Location = new System.Drawing.Point(456, 121);
+            this.textMail.Name = "textMail";
+            this.textMail.Size = new System.Drawing.Size(149, 22);
+            this.textMail.TabIndex = 10;
+            // 
+            // comboLevel
+            // 
+            this.comboLevel.FormattingEnabled = true;
+            this.comboLevel.Items.AddRange(new object[] {
+            "Beginner",
+            "Admin",
+            "Ex-Member",
+            "Co-Manager",
+            "Manager",
+            "Standard"});
+            this.comboLevel.Location = new System.Drawing.Point(456, 150);
+            this.comboLevel.Name = "comboLevel";
+            this.comboLevel.Size = new System.Drawing.Size(149, 24);
+            this.comboLevel.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.name,
+            this.surname,
+            this.mail,
+            this.level});
+            this.dataGridView1.Location = new System.Drawing.Point(30, 203);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(685, 218);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(375, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Id";
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(456, 35);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(149, 22);
+            this.textId.TabIndex = 14;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Width = 125;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.Width = 125;
+            // 
+            // surname
+            // 
+            this.surname.HeaderText = "Surname";
+            this.surname.MinimumWidth = 6;
+            this.surname.Name = "surname";
+            this.surname.Width = 125;
+            // 
+            // mail
+            // 
+            this.mail.HeaderText = "Mail";
+            this.mail.MinimumWidth = 6;
+            this.mail.Name = "mail";
+            this.mail.Width = 125;
+            // 
+            // level
+            // 
+            this.level.HeaderText = "Level";
+            this.level.MinimumWidth = 6;
+            this.level.Name = "level";
+            this.level.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboLevel);
+            this.Controls.Add(this.textMail);
+            this.Controls.Add(this.textSurname);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "MEMBERS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textSurname;
+        private System.Windows.Forms.TextBox textMail;
+        private System.Windows.Forms.ComboBox comboLevel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn level;
+    }
+}
+
---
+++ Veritabani/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Path of database
+        string path = "members.db";
+        string cs = $"URI=file:{Application.StartupPath}\\members.db"; // Database created in the debug folder
+        SQLiteConnection con;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void data_show()
+        {
+            dataGridView1.Rows.Clear();
+
+            using (var con = new SQLiteConnection(cs))
+            {
+                con.Open();
+
+                string sql = "SELECT * FROM member";
+                using (var cmd = new SQLiteCommand(sql, con))
+                {
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            dataGridView1.Rows.Add(dr["Id"], dr["Name"], dr["Surname"], dr["Mail"], dr["Level"]);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Create database and table
+        private void Create_db()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+
+                using (var sqlite = new SQLiteConnection($"Data Source={path}"))
+                {
+                    sqlite.Open();
+                    string sql = "CREATE TABLE member(Id varchar(50), Name varchar(20), Surname varchar(20), Mail varchar(20), [Level] varchar(20))";
+                    using (var command = new SQLiteCommand(sql, sqlite))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Database already exists");
+            }
+        }
+
+        // Insert Data
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var con = new SQLiteConnection(cs))
+            {
+                con.Open();
+                using (var cmd = new SQLiteCommand("INSERT INTO member(Id,Name,Surname,Mail,[Level]) VALUES(@Id,@Name,@Surname,@Mail,@Level)", con))
+                {
+                    try
+                    {
+                        string ID = textId.Text;
+                        string NAME = textName.Text;
+                        string SURNAME = textSurname.Text;
+                        string MAIL = textMail.Text;
+                        string LEVEL = comboLevel.Text;
+
+                        cmd.Parameters.AddWithValue("@Id", ID);
+                        cmd.Parameters.AddWithValue("@Name", NAME);
+                        cmd.Parameters.AddWithValue("@Surname", SURNAME);
+                        cmd.Parameters.AddWithValue("@Mail", MAIL);
+                        cmd.Parameters.AddWithValue("@Level", LEVEL);
+
+                        dataGridView1.ColumnCount = 5;
+                        dataGridView1.Columns[0].Name = "Id";
+                        dataGridView1.Columns[1].Name = "Name";
+                        dataGridView1.Columns[2].Name = "Surname";
+                        dataGridView1.Columns[3].Name = "Mail";
+                        dataGridView1.Columns[4].Name = "Level";
+
+                        string[] row = new string[] { ID, NAME, SURNAME, MAIL, LEVEL };
+                        dataGridView1.Rows.Add(row);
+
+
+                        cmd.ExecuteNonQuery();
+                        data_show();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Cannot insert data. Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        // Update Data
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (var con = new SQLiteConnection(cs))
+            {
+                con.Open();
+                using (var cmd = new SQLiteCommand("UPDATE member SET Name=@Name, Surname=@Surname, Mail=@Mail, [Level]=@Level WHERE Id=@Id", con))
+                {
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@Id", textId.Text);
+                        cmd.Parameters.AddWithValue("@Name", textName.Text);
+                        cmd.Parameters.AddWithValue("@Surname", textSurname.Text);
+                        cmd.Parameters.AddWithValue("@Mail", textMail.Text);
+                        cmd.Parameters.AddWithValue("@Level", comboLevel.Text);
+
+                        cmd.ExecuteNonQuery();
+                        data_show();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Cannot update data. Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        // Delete Data
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (var con = new SQLiteConnection(cs))
+            {
+                con.Open();
+                using (var cmd = new SQLiteCommand("DELETE FROM member WHERE Id=@Id", con))
+                {
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@Id", textId.Text);
+
+                        cmd.ExecuteNonQuery();
+                        data_show();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Cannot delete data. Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        // Show Data
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            data_show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create_db();
+        }
+    }
+}
